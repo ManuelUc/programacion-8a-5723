cf415415c3b1e76bf4b1dc4793c4fc01d7d04756
manueluc <manueluc172@gmail.com>
2020-02-19 08:59:36 -0600

    practicando

---
+++ oprbasicas/act14.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int x1=2;
+	int x2=4;
+	int x3=6;
+	int x4=8;
+	int x5=10;
+	int x6=12;
+	int x7=14;
+	int x8=16;
+	int x9=18;
+	int x10=20;
+	int s;
+
+	s=x1+x2+x3+x4+x5+x6+x7+x8+x9+10;
+	cout<<"la suma de los primeros 10 numeros pares es "<<s<<"";
+	return 0;
+}
---
+++ oprbasicas/act8.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int x1, x2, x3, x4, x5, x6, x7, x8, x9, x10;
+	int y;
+
+	cout<<"ingresar numeros a sumar:";
+	cin>>x1>>x2>>x3>>x4>>x5>>x6>>x7>>x8>>x9>>x10;
+	y=x1+x2+x3+x4+x5+x6+x7+x8+x9+x10;
+	cout<<"el resultado es:"<<y<<"";
+	return 0;
+}
